67520ba743174d9e15c554841eb0732a7f9b0372
RandomName08 <quentinpison@gmail.com>
2023-07-06 17:54:38 +0800

    update

--- LettuceMOT.py
+++ LettuceMOT.py
@@ -2,9 +2,21 @@ from matplotlib import image
 from matplotlib import pyplot
 import numpy
 from PIL import Image
+import PIL
+import cv2
+import os
+import glob
 
-test = open('B&F1\gt.txt')
+test = open('B&F2\gt.txt') #change the 2 to 1 when cropping a B&F1
 line = test.readline()
+img_dir = "B&F2\img" #change the 2 to 1 when cropping a B&F1
+crop_dir = "cropped 2" #change the 2 to 1 when cropping a B&F1
+data_path = os.path.join(img_dir,'*g')
+files = glob.glob(data_path)
+photos = []
+for f1 in files:
+    img = cv2.imread(f1)
+    photos.append(img)
 while line:
     print(line)
     data = line.split(',')
@@ -14,16 +26,18 @@ while line:
     ycor = data[3]
     width = data[4]
     height = data[5]
-    im = image.imread("B&F1\img\otest.png")
+    imgiduse = int(imgid) - 1
+    im = photos[imgiduse]
     xuse = int(xcor)
     yuse = im.shape[0] - int(ycor)
     widuse = xuse + int(width)
-    heuse = yuse + int(height)
+    heuse = yuse - int(height)
     print(imgid, letid, xcor, ycor, width, height)
     print(xuse, heuse)
     print(widuse, yuse)
-    cropped = im[yuse:heuse, xuse:widuse]
-    pyplot.imshow(cropped)
-    pyplot.show()
+    cropped = im[heuse:yuse, xuse:widuse]
+    tosave = Image.fromarray(cropped)
+    filename = imgid + '-' + letid
+    tosave.save(f"{crop_dir}/{filename}.png")
     line = test.readline()
 test.close()
